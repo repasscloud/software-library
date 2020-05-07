b5d5da872227926fe84dac04cfb7b78e080ec689
Danijel-James Wynyard <danijel.james@me.com>
2020-05-07 17:15:04 +1000

    update file DL script

---
+++ lib/scripts/Test-FileDownloads.ps1
@@ -0,0 +1,59 @@
+# Remove the files we had previously
+$appDL = '/Users/danijeljames/Developer/software-matrix/.public/'
+Get-ChildItem -Path $appDL | ForEach-Object { Remove-Item -Path $_.FullName -Confirm:$false -Force}
+
+
+# This function was designed to pull down a file from even a redirected URL via PowerShell
+function Get-RedirectedUrl() {
+    
+  param(
+    [Parameter(Mandatory = $true, Position = 0)]
+    [uri] $Url,
+    [Parameter(Position = 1)]
+    [Microsoft.PowerShell.Commands.WebRequestSession] $Session = $null
+  )
+
+  $request_url = $Url
+  $retry = $false
+  
+  do {
+    try {
+      $response = Invoke-WebRequest -Method Head -WebSession $Session -Uri $request_url
+
+      if($response.BaseResponse.ResponseUri -ne $null)
+      {
+        # PowerShell 5
+        $result = $response.BaseResponse.ResponseUri.AbsoluteUri
+      } elseif ($response.BaseResponse.RequestMessage.RequestUri -ne $null) {
+        # PowerShell Core
+        $result = $response.BaseResponse.RequestMessage.RequestUri.AbsoluteUri
+      }
+
+      $retry = $false
+    } catch {
+      if(($_.Exception.GetType() -match "HttpResponseException") -and
+        ($_.Exception -match "302"))
+      {
+        $request_url = $_.Exception.Response.Headers.Location.AbsoluteUri
+        $retry = $true
+      } else {
+        throw $_
+      }
+    }  
+  } while($retry)
+
+  return $result
+}
+
+function Invoke-PullDownThatFile() {  
+  param(
+    [Parameter(Mandatory = $true, Position = 0)]
+    [uri] $URI
+  )
+  $UrlX = Get-RedirectedUrl -Url $URI
+  Invoke-Webrequest -Uri $UrlX -OutFile $('/Users/danijeljames/Developer/software-matrix/.public/' + (Split-Path -Path $UrlX -Leaf))
+}
+
+'' | ForEach-Object { Invoke-PullDownThatFile -URI $_ }
+
+Get-ChildItem -Path $appDL | ForEach-Object { Get-FileHash -Path $_.FullName }
