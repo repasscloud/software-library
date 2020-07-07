bfa97dc84a4fe8dbc2e2df8c591278a80450ed62
Danijel-James Wynyard <danijel.james@me.com>
2020-07-07 18:17:23 +1000

    function clarified, v2.0.0.1, issue #24

--- lib/scripts/functions/Get-URLStatusCode.ps1
+++ lib/scripts/functions/Get-URLStatusCode.ps1
@@ -4,7 +4,19 @@ Copyright 2020 RePass Cloud Pty Ltd
 
 This product includes software developed at
 RePass Cloud (https://repasscloud.com/).
+
+Version: 2.0.0.1
 #>
+
+# Stems from issue #24
+# Returns the Status Code of a web URL that is passed in as an arg
+# Should always return a 200 result to be successful
+# List of URL status codes can be found here: https://www.w3.org/Protocols/HTTP/HTRESP.html
+
+# Usage:
+#
+#    $query = Get-URLStatusCode -Url https://www.com/
+#    $query
 
 
 function Get-UrlStatusCode() {
