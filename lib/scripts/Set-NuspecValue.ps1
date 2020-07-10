3ccff2f18bea34fe2d74ddac615e6f870dc6a307
Danijel-James Wynyard <danijel.james@me.com>
2020-07-11 01:07:45 +1000

    add nuspecvalue function

---
+++ lib/scripts/Set-NuspecValue.ps1
@@ -0,0 +1,16 @@
+# Set Nuspec value
+function Set-NuspecValue {
+    param(
+        [Parameter(Mandatory=$false, Position=0)]
+        [ValidateSet($false,$true)]
+        [System.Boolean]
+        $NuspecFile
+    )
+    if (-not($NuspecFile)) {
+        [String]$return_value='    "Nuspec": false,'
+        return $return_value
+    } else {
+        [String]$return_value='    "Nuspec": true,'
+        return $return_value
+    }
+}
